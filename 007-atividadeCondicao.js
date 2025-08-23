05840bce0a699fac0e730833c435981f9f961e59
Luis Fernando Rodrigues Bueno <77luisnando@gmail.com>
2025-08-22 22:48:25 -0300

    atividade condicao 2

--- 007-atividadeCondicao.js
+++ 007-atividadeCondicao.js
@@ -5,8 +5,12 @@ let nome = entrada('Digite seu nome: ');
 let idade = entrada('Digite sua idade: ');
 let amigo = entrada(`Você é amigo do dono (sim/não)? `);
 
+if (nome === '' || idade === '' || amigo === ''){ // Verificação de dados >> 22/08/2025 ; 22:45
+    console.log(' ');
+    console.log('Por favor, preencha todos os campos corretamente!');
+}
 
-if (amigo == 'sim' || amigo == 'Sim' || amigo == 'SIM' || amigo == 's' || amigo == 'S'){
+else if (amigo == 'sim' || amigo == 'Sim' || amigo == 'SIM' || amigo == 's' || amigo == 'S'){
     if (isNaN(idade) >= 18){
         console.log(' ');
         console.log(`${nome}, você teve o acesso liberado e uma bebida grátis por ser maior de idade e amigo do dono!`);
