7ca966e32074f38e745a9c9fb2cdcd5cd648ae67
Luis Fernando Rodrigues Bueno <77luisnando@gmail.com>
2025-08-22 22:39:49 -0300

    atividade condicao

--- 006-condicao.js
+++ 006-condicao.js
@@ -6,12 +6,9 @@ let nome = entrada('Digite seu nome: ');
 let idade = entrada('Digite sua idade: ');
 console.log(' ');
 
-
 if (isNaN(idade)) {
     console.log(`${nome}, insira um valor numérico para a idade: `);
-} 
-
-else {
+} else {
     if (idade >= 18) {
         console.log(`${nome}, você é maior de idade.`);
     }
@@ -20,3 +17,10 @@ else {
         console.log(`${nome}, você é menor de idade.`);
     }
 }
+
+if (nome.length > 5){
+    console.log('Seu nome é muito curto.');
+}
+
+const maioridade = idade >= 18 ? 'Maior' : 'Menor'; // Ternário - O uso de uma váriavel para armazenar o resultado de uma condição >> 22/08/25 ; 21:46
+console.log(`${nome}, descobri que voce é ${maioridade} de idade.`);
---
+++ 007-atividadeCondicao.js
@@ -0,0 +1,29 @@
+//Aula >> 22/08/2025 ; 22:24
+
+let entrada = require('prompt-sync')();
+let nome = entrada('Digite seu nome: ');
+let idade = entrada('Digite sua idade: ');
+let amigo = entrada(`Você é amigo do dono (sim/não)? `);
+
+
+if (amigo == 'sim' || amigo == 'Sim' || amigo == 'SIM' || amigo == 's' || amigo == 'S'){
+    if (isNaN(idade) >= 18){
+        console.log(' ');
+        console.log(`${nome}, você teve o acesso liberado e uma bebida grátis por ser maior de idade e amigo do dono!`);
+    }
+
+    else {
+        console.log(' ');
+        console.log(`${nome}, você teve o acesso liberado por ser amigo do dono!`);
+    }
+} 
+
+else if (isNaN(idade) >= 18){
+        console.log(' ');
+        console.log(`${nome}, você teve o acesso liberado por ser maior de idade!`);
+}   
+
+else {
+    console.log(' ');
+    console.log (`${nome}, você teve o acesso negado por ser menor de idade e não ser amigo do dono!`);
+}
