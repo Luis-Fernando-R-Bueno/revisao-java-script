85be2483ca73f89404afd379c41960b9b8ef4380
Luis Fernando Rodrigues Bueno <77luisnando@gmail.com>
2025-08-25 21:22:10 -0300

    atividade 25/08/2025

--- 007-atividadeCondicao.js
+++ 007-atividadeCondicao.js
@@ -1,33 +1,25 @@
 //Aula >> 22/08/2025 ; 22:24
 
+console.log(' ');
 let entrada = require('prompt-sync')();
 let nome = entrada('Digite seu nome: ');
 let idade = entrada('Digite sua idade: ');
 let amigo = entrada(`Você é amigo do dono (sim/não)? `);
 
 if (nome === '' || idade === '' || amigo === ''){ // Verificação de dados >> 22/08/2025 ; 22:45
-    console.log(' ');
-    console.log('Por favor, preencha todos os campos corretamente!');
+    console.log('\nPor favor, preencha todos os campos corretamente!');
 }
 
-else if (amigo == 'sim' || amigo == 'Sim' || amigo == 'SIM' || amigo == 's' || amigo == 'S'){
-    if (isNaN(idade) >= 18){
-        console.log(' ');
-        console.log(`${nome}, você teve o acesso liberado e uma bebida grátis por ser maior de idade e amigo do dono!`);
+else if (isNaN(idade) >= 18){
+    if (amigo == 'sim' || amigo == 'Sim' || amigo == 'SIM' || amigo == 's' || amigo == 'S'){
+        console.log(`\n${nome}, você teve o acesso liberado e uma bebida grátis por ser maior de idade e amigo do dono!`);
     }
 
     else {
-        console.log(' ');
-        console.log(`${nome}, você teve o acesso liberado por ser amigo do dono!`);
+        console.log(`\n${nome}, você teve o acesso liberado por ser maior de idade!`);
     }
-} 
-
-else if (isNaN(idade) >= 18){
-        console.log(' ');
-        console.log(`${nome}, você teve o acesso liberado por ser maior de idade!`);
 }   
 
 else {
-    console.log(' ');
-    console.log (`${nome}, você teve o acesso negado por ser menor de idade e não ser amigo do dono!`);
+    console.log (`\n${nome}, você teve o acesso negado por ser menor de idade e não ser amigo do dono!`);
 }
---
+++ 009-repeticao.js
@@ -0,0 +1,13 @@
+//Aula >> 25/08/2025 ; 20:34
+
+let n_inicio = 10;
+let n_fim = 20;
+let repeticoes = 0;
+
+while (n_inicio <= n_fim){
+    console.log(n_inicio);
+    n_inicio++;
+    repeticoes++;
+};
+
+console.log(`O total de repetições foi de ${repeticoes} vezes.`);
